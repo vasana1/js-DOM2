61ac6739b17031cd44ca64ea096f8a8bf7fc2f85
Victor <viclee02@gmail.com>
2017-01-04 17:59:28 -1000

    adds updated exercise 5

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 console.log("hi");
+
+
+var nameElement = document.getElementById("name5");
+nameElement.innerHTML = "5. Heinz Corp";
+
+var rateElement = document.getElementById("rating5");
+rateElement.innerHTML = "Rating: 5 Stars";
+
+var priceElement = document.getElementById("price5");
+priceElement.innerHTML = "Price: $";
+
+var addyElement = document.getElementById("addy5");
+addyElement.innerHTML = "Times Supermarket";
+
+var hourElement = document.getElementById("hours5");
+hourElement.innerHTML = "Hours: 9am - 10pm";
+
+
+/*var element6 = document.createElement("div");
+element6.id = "result6";
+main.appendChild(element6);
+
+var pho6 = document.createElement("div");
+pho6.id = "photo6";
+element6.appendChild(pho6);
+
+var img6 = document.createElement("img");
+img6.id = "pic6";
+img6.src = "https://i1.wp.com/diversedinners.com/wp-content/uploads/2015/03/Oxtail-Soup.jpg";
+pho6.appendChild(img6);*/
