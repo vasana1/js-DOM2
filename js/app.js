06b23d02ab600d09c91080c6af0da42d2827f53e
Victor <viclee02@gmail.com>
2017-01-10 11:40:11 -1000

    update assignment

--- js/app.js
+++ js/app.js
@@ -1,25 +1,22 @@
-/*You will need to refer to the index.html page to complete the exercises below. Using DOM, please make the necessary changes to the website.*/
+/*You will need to refer to the index.html page to complete the exercises below. Using DOM, please make the necessary changes to the website.
+
+Also don't forget to run your live-server for this assignment.*/
 
 //1. Kapiolani Coffee Shop
-/*Despite being top dog, Kapiolani Coffee Shop's current rating is n/a. Let's keep the hungry fans happy by changing the current ranking to:
+/*Despite being the cat's meow, Kapiolani Coffee Shop's current rating is n/a. Let's keep the hungry fans happy by changing the current ranking to:
 
-Rating: 5 Stars
-*/
+Rating: 5 Stars*/
 
-var ratingElement = document.getElementById("rating1").innerHTML = "Rating: 5 Stars";
 
 //2. Sunrise Restaurant
-/*No rise and shine for the Sunrise Restaurant, as their price and address needs some serious updating. Change the price and address to the following:
+/*No rise and shine for the Sunrise Restaurant, as their price and address needs some serious updating. Bring in some sunshine by changing the price and address to the following:
 
 Price: $$$
 Adress: 525 Kapahulu Ave, Honolulu, HI 96815*/
 
-var priceElement = document.getElementById("price2").innerHTML = "Price: $$$";
-
-var addyElement = document.getElementById("addy2").innerHTML = "Address: 525 Kapahulu Ave, Honolulu, HI 96815";
 
 //3. Ramen Nakamura
-/*Literally, where did the time go? Ramen Nakamura is missing the hours section! This mistake is timeless...
+/*Literally, where did the time go? Ramen Nakamura is missing the hours section! This mistake is timeless.
 
 You will need to create a new div element with an id of "hours3". Inside this div element, give it the following info:
 
@@ -27,14 +24,9 @@ Hours: 6pm - 12am Sun - Sat
 
 Append this div element to the div id "text3"*/
 
-var hourElement = document.createElement("div");
-hourElement.id = "hours3";
-hourElement.innerHTML = "Hours: 6pm - 12am Sun - Sat";
-text3.appendChild(hourElement);
-
 
 //4. Kam Bowl
-/*Kam Bowl got no spare time to update their info and business is suffering! Help them be the kingpin again.
+/*Kam Bowl got no spare time to update their info and business is suffering! Help them regain their kingpin status again.
 
 Create a new div element with an id of "price4". Inside this div element, give it the following info:
 
@@ -43,22 +35,12 @@ Price: $$$
 Append this element to the div with the id of 
 "text4"*/
 
-var price4Elem = document.createElement("div");
-price4Elem.id = "price4";
-price4Elem.innerHTML = "Price: $$$";
-text4.appendChild(price4Elem);
-
 /*Create a new div element with an id of "addy4". Inside this div element, give it the following info:
 
 Address: 1620 N. School St, Honolulu, HI 96817
 
 Append this element to the div with the id of 
 "text4"*/
-
-var addy4Elem = document.createElement("div");
-addy4Elem.id = "addy4";
-addy4Elem.innerHTML = "Address: 1620 N. School St, Honolulu, HI 96817";
-text4.appendChild(addy4Elem);
 
 /*Create a new div element with an id of "hours4". Inside this div element, give it the following info:
 
@@ -68,24 +50,32 @@ Append this element to the div with the id of
 "text4"
 */
 
-
-var hours4Elem = document.createElement("div");
-hours4Elem.id = "hours4";
-hours4Elem.innerHTML = "Hours: 9am - 10pm Sun - Sat";
-text4.appendChild(hours4Elem);
-
-
 /*Create a paragraph element with an id of "update4". Inside this paragraph element, give it the following info:
 
 I'd leave a blank space for this oxtail soup!
 
 Append this element to the div id of "comment4"*/
 
-var pElement = document.createElement('p');
-pElement.id = "update4";
-pElement.innerHTML = "I'd leave a blank space for this oxtail soup!";
-comment4.appendChild(pElement);
 
+//5. The Death Star Cafe
+/*The empire strikes back with a new and improved oxtail soup offering. Help the dark side win back customers by executing the following commands:*/
 
+/*Create the div elements with the following id's:
 
+"name5"
+"rating5"
+"price5"
+"addy5"
+"hours5"
+
+Add the following info for each specific div id:
+
+name5: 5. The Death Star Cafe
+rating5: Rating: 2 Stars
+price5: Price: $$
+addy5: Address: A galaxy far, far away.
+hours5: Hours: We never close.
+
+Append the div elements to the div id "text5"
+*/
 
